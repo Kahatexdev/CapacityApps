19830dd4d0a82467a12d389d13d5c6f3d33fd3b0
Andiyogop <163397996+Andiyogop@users.noreply.github.com>
2024-05-27 11:36:37 +0700

    ubah struktur data planning & perbarui operation target

---
+++ app/Database/Migrations/2024-05-27-030200_AddEstimatedPlanning.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class AddEstimatedPlanning extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id_est_qty' => [
+                'type' => 'INT',
+                'constraint' => 11,
+                'unsigned' => true,
+                'auto_increment' => true,
+            ],
+            'id_detail_pln' => [
+                'type' => 'INT',
+                'constraint' => 11,
+            ],
+            'Est_qty' => [
+                'type' => 'INT',
+                'constraint' => 25,
+            ],
+            'hari' => [
+                'type' => 'INT',
+                'constraint' => 11,
+            ],
+        ]);
+        $this->forge->addKey('id_est_qty', true);
+        $this->forge->createTable('estimated_planning');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('estimated_planning');
+    }
+}
