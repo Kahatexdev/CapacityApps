f0bcb1004ac3c87e9b58351a9594fc1fc7c59790
Andiyogop <163397996+Andiyogop@users.noreply.github.com>
2024-05-30 13:07:48 +0700

    tambah all data booking

--- app/Views/Capacity/Booking/booking.php
+++ app/Views/Capacity/Booking/booking.php
@@ -132,6 +132,29 @@
                 </a>
             </div>
         <?php endforeach ?>
+        <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4 mt-2">
+                <a href="<?= base_url('capacity/allbooking/')?>">
+                    <div class="card">
+                        <div class="card-body p-3">
+                            <div class="row">
+
+                                <div class="col-8">
+                                    <div class="numbers">
+                                        <p class="text-sm mb-0 text-capitalize font-weight-bold">Semua Data Booking</p>
+                                        <h5 class="font-weight-bolder mb-0">
+                                        </h5>
+                                    </div>
+                                </div>
+                                <div class="col-4 text-end">
+                                    <div class="icon icon-shape bg-gradient-info shadow text-center border-radius-md">
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </div>
 
     </div>
 
