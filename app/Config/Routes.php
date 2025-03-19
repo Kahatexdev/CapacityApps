7f834b57556977613c588579e1717aaeae437850
notfoundra <novandram15@gmail.com>
2025-03-19 13:10:58 +0700

    merging

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -432,9 +432,6 @@ $routes->group('/user', ['filter' => 'user'], function ($routes) {
     $routes->get('viewModelPlusPacking/(:any)', 'ProduksiController::viewModelPlusPacking/$1');
     $routes->get('pluspacking', 'ProduksiController::pluspacking');
     $routes->post('inputpo', 'ProduksiController::updatepo');
-    $routes->post('get-area', 'ProduksiController::getArea');
-    $routes->post('get-size', 'ProduksiController::getSize');
-    $routes->post('prosesInputProdManual', 'ProduksiController::inputProduksiManual');
 
     //summary
     $routes->post('summaryproduksi', 'ProduksiController::summaryProduksi');
@@ -443,6 +440,10 @@ $routes->group('/user', ['filter' => 'user'], function ($routes) {
     //summary produksi
     $routes->post('summaryProdPerTanggal', 'ProduksiController::summaryProdPerTanggal');
     $routes->post('exportSummaryPerTgl', 'SummaryController::excelSummaryPerTgl');
+
+    $routes->get('datadeffect', 'DeffectController::datadeffect');
+    $routes->post('inputKode', 'DeffectController::inputKode');
+    $routes->post('viewDataBs', 'DeffectController::viewDataBs');
 
     //timter produksi
     $routes->post('timterProduksi', 'ProduksiController::timterProduksi');
@@ -468,8 +469,6 @@ $routes->group('/user', ['filter' => 'user'], function ($routes) {
     $routes->get('bahanBaku/hapusSession', 'MaterialController::deleteAllPemesananSession');
     $routes->get('bahanBaku/hapusSession/(:any)/(:any)', 'MaterialController::deletePemesananSession/$1/$2');
     $routes->get('listPemesanan/(:any)', 'MaterialController::listPemesanan/$1');
-
-    //pph
 });
 
 // sudo
@@ -546,15 +545,10 @@ $routes->group('/sudo', ['filter' => 'sudo', 'god'], function ($routes) {
     $routes->get('detailproduksi/(:any)', 'ProduksiController::produksiPerArea/$1');
     $routes->post('detailproduksi/(:any)', 'ProduksiController::produksiPerArea/$1');
     $routes->get('updatebs', 'ProduksiController::updatebs');
-    $routes->get('hapus-produksi/(:any)', 'ProduksiController::deleteProduksi/$1');
 
     $routes->post('summaryproduksi', 'ProduksiController::summaryProduksi');
     $routes->get('bssetting', 'UserController::bssetting');
     $routes->post('importbssetting', 'ProduksiController::importbssetting');
-    $routes->post('get-area', 'ProduksiController::getArea');
-    $routes->post('get-size', 'ProduksiController::getSize');
-    $routes->post('prosesInputProdManual', 'ProduksiController::inputProduksiManual');
-
 
     //summary
     $routes->post('summaryproduksi', 'ProduksiController::summaryProduksi');
--- app/Views/User/layout.php
+++ app/Views/User/layout.php
@@ -189,11 +189,11 @@
                 </li>
 
                 <li class="nav-item mt-3  ">
-                    <h6 class="ps-4 ms-2 text-uppercase text-xs font-weight-bolder opacity-6">Master Data</h6>
+                    <h6 class="ps-4 ms-2 text-uppercase text-xs font-weight-bolder opacity-6">Deffect</h6>
                 </li>
 
                 <li class="nav-item">
-                    <a class="nav-link  " href="<?= base_url('user/bssetting') ?>">
+                    <a class="nav-link  " href="<?= base_url('user/datadeffect') ?>">
                         <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
                             <svg width="12px" height="12px" viewBox="0 0 40 44" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
                                 <title>document</title>
@@ -211,7 +211,7 @@
                                 </g>
                             </svg>
                         </div>
-                        <span class="nav-link-text ms-1">BS Setting</span>
+                        <span class="nav-link-text ms-1">Data Deffect</span>
                     </a>
                 </li>
                 <li class="nav-item">
