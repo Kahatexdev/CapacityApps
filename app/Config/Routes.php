58909e92f2415438fb228c5af9528f9a81765e66
Alfast456 <alfast456@gmail.com>
2025-04-15 14:25:08 +0700

    retur bahan baku notyet validated

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -519,6 +519,7 @@ $routes->group('/user', ['filter' => 'user'], function ($routes) {
     $routes->get('retur/(:any)', 'ReturController::index/$1');
     $routes->get('filterRetur/(:any)', 'ReturController::dataRetur/$1');
     $routes->post('retur/(:any)/pengajuanRetur', 'ReturController::pengajuanRetur/$1');
+    $routes->get('getKategoriRetur', 'ReturController::getKategoriRetur');
 });
 
 // sudo
