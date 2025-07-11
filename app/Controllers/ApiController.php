bdb0b076ddacb20478ebb6db56b961231e200850
Alfast456 <alfast456@gmail.com>
2025-07-11 16:44:17 +0700

    add api

--- app/Controllers/ApiController.php
+++ app/Controllers/ApiController.php
@@ -435,4 +435,28 @@ class ApiController extends ResourceController
         // Return the data with a 200 status
         return $this->respond($apsData, ResponseInterface::HTTP_OK);
     }
+
+    public function getStartMc($model)
+    {
+        // Validate the input parameter
+        if (!$model) {
+            return $this->response->setJSON([
+                "error" => "Parameter tidak lengkap",
+                "received" => [
+                    "model" => $model,
+                ]
+            ])->setStatusCode(400);
+        }
+
+        // Fetch data from the model
+        $startMc = $this->orderModel->getStartMc($model);
+
+        // Check if data is found
+        if (empty($startMc)) {
+            return $this->respond(['message' => 'Data tidak ditemukan'], ResponseInterface::HTTP_NOT_FOUND);
+        }
+
+        // Return the data with a 200 status
+        return $this->respond($startMc, ResponseInterface::HTTP_OK);
+    }
 }
