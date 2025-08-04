4b36e18641b077ad0b79aa5dd8ac89f30874d32c
notfoundra <novandram15@gmail.com>
2025-08-04 08:24:39 +0700

    fix machine

---
+++ app/Database/Migrations/2025-08-02-040808_PdkStockTable.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class PdkStockTable extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id_stok_pdk' => [
+                'type' => 'INT',
+                'constraint' => 11,
+                'unsigned' => true,
+                'auto_increment' => true,
+            ],
+            'mastermodel' => [
+                'type' => 'VARCHAR',
+                'constraint' => 35,
+            ],
+            'size' => [
+                'type' => 'VARCHAR',
+                'constraint' => 35,
+            ],
+            'delivery' => [
+                'type' => 'date',
+            ],
+            'qty_asli' => [
+                'type' => 'int',
+                'constraint' => 50,
+            ],
+            'stok' => [
+                'type' => 'int',
+                'constraint' => 50,
+            ],
+            'qty_akhir' => [
+                'type' => 'int',
+                'constraint' => 50,
+            ],
+
+        ]);
+        $this->forge->addKey('id_stok_pdk', true);
+        // Tambahkan kunci asing ke sisa tabel referensi di sini
+        // $this->forge->addForeignKey('id_booking', 'data_booking', 'id_booking', 'CASCADE', 'CASCADE');
+        // $this->forge->addForeignKey('id_product_type', 'master_product_type', 'id_product_type');
+        $this->forge->createTable('stok_pdk');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('stok_pdk');
+    }
+}
