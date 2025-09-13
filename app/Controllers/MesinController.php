7630b106212c8c3c8ec650b29dc368db1bf2a45e
notfoundra <novandram15@gmail.com>
2025-09-13 09:29:47 +0700

    capacity Area

--- app/Controllers/MesinController.php
+++ app/Controllers/MesinController.php
@@ -692,7 +692,6 @@ class MesinController extends BaseController
         $weeklyProduction = array_fill($startWeek, 12, 0);
         $weeklyMachines = array_fill($startWeek, 12, 0);
         $weeklySisa = array_fill($startWeek, 12, 0);
-        dd($capacity);
         foreach ($capacity as $row) {
             $pdk = $row['mastermodel'];
             $delivery = $row['delivery'];
