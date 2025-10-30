a88e7cc10ac8876f901d491ae366c81a6c2fd0ae
notfoundra <novandram15@gmail.com>
2025-10-30 09:02:00 +0700

    fix list pps

--- app/Models/ApsPerstyleModel.php
+++ app/Models/ApsPerstyleModel.php
@@ -1477,6 +1477,7 @@ class ApsPerstyleModel extends Model
             ->join('pps', 'pps.id_mesin_perinisial = mesin_perinisial.id_mesin_perinisial', 'left')
             ->join('data_model', 'data_model.no_model = apsperstyle.mastermodel', 'left')
             ->where('apsperstyle.mastermodel', $pdk)
+            ->where('apsperstyle.qty >', 0)
             ->groupBy('apsperstyle.size')
             ->findAll();
     }
