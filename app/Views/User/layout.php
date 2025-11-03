85bc80e752bf9bdac622e664f14c4ea6baa86b33
novandra <75214830+notfoundra@users.noreply.github.com>
2025-11-03 23:33:22 +0700

    supermarket area

--- app/Views/User/layout.php
+++ app/Views/User/layout.php
@@ -193,18 +193,19 @@
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link   " href="<?= base_url('user/stockbahanbaku/' . session()->get('username')) ?>">
-
+                    <a data-bs-toggle="collapse" href="#stockMenu" class="nav-link" aria-controls="stockMenu" role="button" aria-expanded="false">
                         <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
-                            <svg width="12px" height="12px" viewBox="0 0 40 44" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                            <svg width="12px" height="12px" viewBox="0 0 40 44" xmlns="http://www.w3.org/2000/svg">
                                 <title>document</title>
-                                <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                                <g stroke="none" fill="none" fill-rule="evenodd">
                                     <g transform="translate(-1870.000000, -591.000000)" fill="#FFFFFF" fill-rule="nonzero">
                                         <g transform="translate(1716.000000, 291.000000)">
                                             <g transform="translate(154.000000, 300.000000)">
-                                                <path class="color-background opacity-6" d="M40,40 L36.3636364,40 L36.3636364,3.63636364 L5.45454545,3.63636364 L5.45454545,0 L38.1818182,0 C39.1854545,0 40,0.814545455 40,1.81818182 L40,40 Z">
+                                                <path class="color-background opacity-6"
+                                                    d="M40,40 L36.36,40 L36.36,3.63 L5.45,3.63 L5.45,0 L38.18,0 C39.18,0 40,0.81 40,1.81 L40,40 Z">
                                                 </path>
-                                                <path class="color-background" d="M30.9090909,7.27272727 L1.81818182,7.27272727 C0.814545455,7.27272727 0,8.08727273 0,9.09090909 L0,41.8181818 C0,42.8218182 0.814545455,43.6363636 1.81818182,43.6363636 L30.9090909,43.6363636 C31.9127273,43.6363636 32.7272727,42.8218182 32.7272727,41.8181818 L32.7272727,9.09090909 C32.7272727,8.08727273 31.9127273,7.27272727 30.9090909,7.27272727 Z M18.1818182,34.5454545 L7.27272727,34.5454545 L7.27272727,30.9090909 L18.1818182,30.9090909 L18.1818182,34.5454545 Z M25.4545455,27.2727273 L7.27272727,27.2727273 L7.27272727,23.6363636 L25.4545455,23.6363636 L25.4545455,27.2727273 Z M25.4545455,20 L7.27272727,20 L7.27272727,16.3636364 L25.4545455,16.3636364 L25.4545455,20 Z">
+                                                <path class="color-background"
+                                                    d="M30.9,7.27 L1.81,7.27 C0.81,7.27 0,8.08 0,9.09 L0,41.81 C0,42.82 0.81,43.63 1.81,43.63 L30.9,43.63 C31.91,43.63 32.72,42.82 32.72,41.81 L32.72,9.09 C32.72,8.08 31.91,7.27 30.9,7.27 Z M18.18,34.54 L7.27,34.54 L7.27,30.9 L18.18,30.9 L18.18,34.54 Z M25.45,27.27 L7.27,27.27 L7.27,23.63 L25.45,23.63 L25.45,27.27 Z M25.45,20 L7.27,20 L7.27,16.36 L25.45,16.36 L25.45,20 Z">
                                                 </path>
                                             </g>
                                         </g>
@@ -214,7 +215,25 @@
                         </div>
                         <span class="nav-link-text ms-1">Stock Bahan Baku</span>
                     </a>
+
+                    <div class="collapse" id="stockMenu">
+                        <ul class="nav ms-4">
+                            <li class="nav-item">
+                                <a class="nav-link" href="<?= base_url('user/stockbahanbaku/' . session()->get('username')) ?>">
+                                    <span class="sidenav-mini-icon">•</span>
+                                    <span class="sidenav-normal">Stock GBN</span>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="<?= base_url('user/stockarea/' . session()->get('username')) ?>">
+                                    <span class="sidenav-mini-icon">•</span>
+                                    <span class="sidenav-normal">Stock Area</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
                 </li>
+
                 <li class="nav-item">
                     <a class="nav-link   " href="<?= base_url('user/pph/' . session()->get('username')) ?>">
 
